f4d698f4039ae89e5b738b307f0c81acc95c55b4
diegoferreira <dgnnsx@gmail.com>
2018-02-04 23:51:22 -0200

    [Parcial] Carregamento de vários programas na memória de instruções

--- componentes_CPU/mmu.v
+++ componentes_CPU/mmu.v
@@ -1,12 +1,11 @@
-module mmu(clk_50, clk, we, sel, lower, upper, addrIn, addrOut);
+module mmu(clk_50, clk, we, sel, offset, addrIn, addrOut);
 	// Entradas
 	input clk_50;
 	input clk;
 	input we;
-	input [15:0] sel;
-	input [31:0] lower;
-	input [31:0] upper;
-	input [25:0] addrIn;								// Endereco logico
+	input [31:0] sel;
+	input [31:0] offset;
+	input [25:0] addrIn;									// Endereco logico
 
 	// Saida
 	output reg [25:0] addrOut;							// Endereco fisico
@@ -16,7 +15,7 @@ module mmu(clk_50, clk, we, sel, lower, upper, addrIn, addrOut);
 	 * Se houver uma tentativa de acesso a um endereco que nao esteja dentro do range, deve ser lancada uma excecao.
 	 */
 	reg [31:0] base[10:0];							// Endereco de memoria que representa o limite superior da area de memoria do processo (lower bound)
-	reg [31:0] bound[10:0];							// Endereco de memoria que representa o limite inferior da area de memoria do processo (upper bound)
+	//reg [31:0] bound[10:0];							// Endereco de memoria que representa o limite inferior da area de memoria do processo (upper bound)
 	reg [31:0] selector;								// Seletor de segmento
 	
 	wire [31:0] aux;									// Auxiliar para realizar a soma do offset do endereco fisico
@@ -24,13 +23,12 @@ module mmu(clk_50, clk, we, sel, lower, upper, addrIn, addrOut);
 	always @ (posedge clk) begin
 		if (we) begin
 			selector <= sel;
-			base[sel] <= lower;
-			bound[sel] <= upper;
+			base[sel] <= offset;
 		end
 	end
 	
 	assign aux = addrIn + base[selector];
 	always @ (posedge clk_50) begin
-		addrOut <= aux <= bound[selector] && aux >= base[selector] ? aux : {32 {1'b0}}; // TODO: Lancar excecao caso caia fora do range
+		addrOut <= aux; //<= bound[selector] && aux >= base[selector] ? aux : {32 {1'b0}}; // TODO: Lancar excecao caso caia fora do range
 	end
 endmodule
--- componentes_CPU/unidade_de_controle/unidade_de_controle.v
+++ componentes_CPU/unidade_de_controle/unidade_de_controle.v
@@ -78,7 +78,8 @@ module unidade_de_controle(isFalse, isInput, rst, rstBios, op, func, regWrite, m
 	
 	wire i_jf					= ~op[5] & op[4] & ~op[3] & op[2] & ~op[1] & op[0];		// 010101
 	
-	wire i_mmu 					= op[5] & ~op[4] & ~op[3] & ~op[2] & ~op[1] & ~op[0];		// 100000
+	wire i_mmu_lower			= op[5] & ~op[4] & ~op[3] & ~op[2] & ~op[1] & ~op[0];		// 100000
+	wire i_mmu_upper			= op[5] & ~op[4] & ~op[3] & ~op[2] & ~op[1] & op[0];		// 100001
 	
 	// J Type
 	wire i_j						= ~op[5] & op[4] & ~op[3] & op[2] & op[1] & ~op[0];		// 010110
@@ -108,7 +109,7 @@ module unidade_de_controle(isFalse, isInput, rst, rstBios, op, func, regWrite, m
 	assign memWrite			= i_sw;
 	assign imWrite				= i_sim;
 	assign diskWrite			= i_sdk;
-	assign mmuWrite			= i_mmu;
+	assign mmuWrite			= i_mmu_lower | i_mmu_upper;
 	assign isRegAluOp 		= i_add  | i_sub  | i_mul  | i_div  | i_mod  |
 									i_and  | i_or   | i_xor  |
 									i_sll  | i_srl  |
@@ -136,14 +137,14 @@ module unidade_de_controle(isFalse, isInput, rst, rstBios, op, func, regWrite, m
 	assign aluOp[1]			= i_mul	| i_div	| i_xor	| i_srl	| i_lt	| i_not	|
 									i_muli	| i_divi	| i_xori | i_srli	| i_let	|
 									i_mov	| i_li	| i_jr	| i_out	| i_jf	|
-									i_ldk	| i_sim	| i_mmu;
+									i_ldk	| i_sim	| i_mmu_lower | i_mmu_upper;
 	assign aluOp[2]			= i_mod	| i_sll	| i_srl	| i_land	| i_lor	| i_gt  	|
 									i_modi	| i_slli	| i_srli	| i_landi| i_lori | i_get 	|
 									i_mov	| i_li	| i_jr	| i_out	| i_jf	|
-									i_ldk	| i_sim	| i_mmu;
+									i_ldk	| i_sim	| i_mmu_lower | i_mmu_upper;
 	assign aluOp[3]			= i_and	| i_or	| i_xor	| i_land	| i_lor	| i_not	|
 									i_andi | i_ori  | i_xori | i_landi| i_lori |
 									i_mov  | i_li	| i_jr	| i_out	| i_jf	|
-									i_ldk	| i_sim	| i_mmu;
+									i_ldk	| i_sim	| i_mmu_lower | i_mmu_upper;
 	assign aluOp[4]			= i_eq 	| i_ne	| i_lt	| i_let	| i_gt	| i_get;
 endmodule
