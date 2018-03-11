31eb8f1f50caecbb15ff19e4ad8ecf669339a046
diegoferreira <dgnnsx@gmail.com>
2018-03-11 15:30:22 -0300

    [Parcial] Escolha do processo protagonista

--- componentes_CPU/modulo_input.v
+++ componentes_CPU/modulo_input.v
@@ -5,6 +5,7 @@ module modulo_input(in, out);
 	output reg [31:0] out; 		// Dado de entrada estendido para 32 bits
 	
 	always @ (*) begin
-		out <= in[17] == 1'b0 ? in + 32'b0 : in + 32'b11111111111111000000000000000000;
+		//out <= in[17] == 1'b0 ? in + 32'b0 : in + 32'b11111111111111000000000000000000;
+		out <= in[7:0] + 32'b0;
 	end
 endmodule
