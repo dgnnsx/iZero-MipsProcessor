e5af322957e6e3b5579946c1be5b5ad1b1c29f6e
diegoferreira <dgnnsx@gmail.com>
2018-03-18 04:14:55 -0300

    Small refact

--- componentes_CPU/watchdog.v
+++ componentes_CPU/watchdog.v
@@ -3,21 +3,18 @@ module watchdog(clk, reset, isUser, irq);
 	input reset;
 	input isUser;
 	
-	output reg irq; 						// Interrupcao
+	output irq; 						// Interrupcao
 	
 	localparam COUNTER_WIDTH = 6;
 	reg [COUNTER_WIDTH-1:0] contador;
 	
 	initial begin
-		irq <= 1'b0;
 		contador <= 0;
 	end
 	
 	always @ (posedge clk) begin
-		contador <= reset ? 0 : contador + 1'b1;
+		contador <= reset ? 6'b0 : contador + 1'b1;
 	end
 	
-	always @ (posedge clk) begin
-		irq <= reset ? 1'b0 : isUser ? contador[COUNTER_WIDTH-1] : 1'b0;
-	end
+	assign irq = isUser ? contador[COUNTER_WIDTH-1] : 1'b0;
 endmodule
