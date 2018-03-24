a5d20bc2ac72a53b4c2903cef09516ebaacf9555
diegoferreira <dgnnsx@gmail.com>
2018-03-23 23:39:49 -0300

    Execução preemptiva com 3 programas

--- componentes_CPU/controlador_interrupcao.v
+++ componentes_CPU/controlador_interrupcao.v
@@ -1,23 +1,24 @@
-module controlador_interrupcao(clk, irq1, pc, ack, clr, intr, cause, pcBckp);
-	// Entradas
-	input clk;
-	input irq1;								// Watchdog
-	input [25:0] pc;						// backup pc
+module controlador_interrupcao(
+	input clk,
+	input irq1,								// Watchdog
+	input ack,								// Reconhecimento da interrupçao
+	input clr,								// Limpar o codigo de interrupçao
+	input [5:0] opcode,
+	input [25:0] pc,						// backup pc
+	output intr,							// Sinal de interrupçao para a Unidade de Controle
+	output reg [31:0] cause,			// Identificaçao da interrupçao
+	output reg [31:0] pcBckp);			// Backup do pc na interrupçao
 	
-	input ack;								// Reconhecimento da interrupçao
-	input clr;								// Limpar o codigo de interrupçao
-	
-	// Saida
-	output intr;							// Sinal de interrupçao para a Unidade de Controle
-	output reg [31:0] cause;			// Identificaçao da interrupçao
-	output reg [31:0] pcBckp;			// Backup do pc na interrupçao
+	localparam JR = 6'b010010, JF = 6'b010101, J = 6'b111100, JTM = 6'b111101, JAL = 6'b111110;	
+	// Se for uma instruçao de salto nao deixa trocar o contexto
+	wire salto = opcode == JR | opcode == JF | opcode == J | opcode == JTM | opcode == JAL;
 	
 	initial begin
 		cause <= 32'b0;
 	end
 	
 	always @ (posedge clk) begin
-		pcBckp <= ack ? pc + 1 : pcBckp;
+		pcBckp <= ack ? salto ? pc : pc + 1 : pcBckp;
 	end
 	
 	always @ (posedge clk) begin
--- componentes_CPU/watchdog.v
+++ componentes_CPU/watchdog.v
@@ -6,18 +6,14 @@ module watchdog(
 	output irq);
 	
 	localparam JR = 6'b010010, JF = 6'b010101, J = 6'b111100, JTM = 6'b111101, JAL = 6'b111110;
-	localparam COUNTER_WIDTH = 5;
-	reg [COUNTER_WIDTH-1:0] contador;
+	localparam COUNTER_WIDTH = 8;
+	reg [COUNTER_WIDTH-1:0] contador = 0;
 	
 	// Se for uma instruçao de salto nao deixa trocar o contexto
 	wire salto = opcode == JR | opcode == JF | opcode == J | opcode == JTM | opcode == JAL;
 	
-	initial begin
-		contador <= 0;
-	end
-	
 	always @ (posedge clk) begin
-		contador <= reset ? {COUNTER_WIDTH{1'b0}} : isUser ? salto ? contador - 1'b1 : contador + 1'b1 : {COUNTER_WIDTH{1'b0}};
+		contador <= reset ? {COUNTER_WIDTH{1'b0}} : isUser ? contador + 1'b1 : {COUNTER_WIDTH{1'b0}};
 	end
 	
 	assign irq = contador[COUNTER_WIDTH-1];
