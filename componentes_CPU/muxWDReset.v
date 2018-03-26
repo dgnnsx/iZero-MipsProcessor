9e1fc2a241d46e250af1ce95e67dd40e60da5fe5
diegoferreira <dgnnsx@gmail.com>
2018-03-26 01:42:07 -0300

    Preempção Funcionando

--- componentes_CPU/muxWDReset.v
+++
@@ -1,7 +0,0 @@
-module muxWDReset(
-	input usrMode,
-	input isInsert,
-	output out);
-	
-	assign out = usrMode | isInsert;
-endmodule
--- componentes_CPU/watchdog.v
+++ componentes_CPU/watchdog.v
@@ -2,13 +2,14 @@ module watchdog(
 	input clk,
 	input reset,
 	input isUser,
+	input isInsert,
 	output irq);
 	
 	localparam COUNTER_WIDTH = 4;
 	reg [COUNTER_WIDTH-1:0] contador = 0;
 	
 	always @ (posedge clk) begin
-		contador <= reset ? {COUNTER_WIDTH{1'b0}} : isUser ? contador + 1'b1 : {COUNTER_WIDTH{1'b0}};
+		contador <= reset ? {COUNTER_WIDTH{1'b0}} : isUser ? isInsert ? contador : contador + 1'b1 : {COUNTER_WIDTH{1'b0}};
 	end
 	
 	assign irq = contador[COUNTER_WIDTH-1];
