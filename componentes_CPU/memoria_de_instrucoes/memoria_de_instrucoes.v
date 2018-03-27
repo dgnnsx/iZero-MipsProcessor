80115fae6dbe8cbdc51d72ac3c945bfd8af7e265
diegoferreira <dgnnsx@gmail.com>
2018-03-26 23:57:14 -0300

    Renomear programa

--- componentes_CPU/memoria_de_instrucoes/memoria_de_instrucoes.v
+++ componentes_CPU/memoria_de_instrucoes/memoria_de_instrucoes.v
@@ -7,7 +7,7 @@ module memoria_de_instrucoes (clk, we, pc, addr, datain, dataout);
 	
 	output reg [31:0] dataout;					// data out (from memory)
 	
-	localparam RAM_SIZE = 2048;				// Tamanho da memoria
+	localparam RAM_SIZE = 4096;				// Tamanho da memoria
 	reg [31:0] ram [RAM_SIZE-1:0];			// ram cells
 	
 	always @ (posedge clk) begin
