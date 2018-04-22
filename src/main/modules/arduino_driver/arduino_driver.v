dc1672283a3dd89a65da3e087975a0c53c2a0b48
diegoferreira <dgnnsx@gmail.com>
2018-04-22 12:24:58 -0300

    Módulo do arduino integrado

--- src/main/modules/arduino_driver/arduino_driver.v
+++ src/main/modules/arduino_driver/arduino_driver.v
@@ -1,25 +1,23 @@
-module arduino_driver(clk50, clk, we, system_input, system_output, module_input, module_output);
-  input clk50;
-  input clk;
-  input we;
-  input [7:0] system_input;
-  input [7:0] module_input;
-  output [31:0] module_output;
+module arduino_driver(
+	input clk50,
+	input clk,
+	input we,
+	input [7:0] system_input,
+	input [31:0] module_input,
+	output reg [7:0] system_output,
+	output [31:0] module_output);
 
-  reg [31:0] system_input_buffer;
-  output reg [7:0] system_output;
+	reg [31:0] system_input_buffer;
+  
+	always @ (posedge clk50) begin
+		system_input_buffer <= {24'b0, system_input};
+	end
 
-  always @ (posedge clk50) begin
-		// TODO: S´O ESTA RECEBENDO 4 BITS- ARRUMAR PARA RECEBER 8
-    system_input_buffer <= {28'b0, system_input};
-  end
+	always @ (posedge clk) begin
+		if (we) begin
+			system_output <= module_input[7:0];
+		end
+	end
 
-  always @ (negedge clk) begin
-    if (we) begin
-      system_output <= module_input;
-    end
-  end
-
-  assign module_output = system_input_buffer;
-
-endmodule // arduino-driver
+	assign module_output = system_input_buffer;
+endmodule
